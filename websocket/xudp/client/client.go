ff10b47c0d6b9e8362f5b776a1e849d542237194
xieruixiang <1046421940@qq.com>
2024-05-15 22:08:19 +0800

    feat:完善udp的websocket

--- websocket/xudp/client/client.go
+++ websocket/xudp/client/client.go
@@ -21,15 +21,13 @@ func main() {
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		readString, _ := reader.ReadString('\n')
-		if strings.ToUpper(strings.Trim("readString", "\r\n")) == "Q" {
+		if strings.ToUpper(strings.Trim(readString, "\r\n")) == "Q" {
 			fmt.Println("退出udp客户端")
 			return
 		}
 		udp.Write([]byte(readString))
 		buf := make([]byte, 1024)
 		n, addr, _ := udp.ReadFromUDP(buf)
-		fmt.Println(addr.String())
-		fmt.Println(udp.RemoteAddr().String())
-		fmt.Println("客户端收到:", string(buf[:n]))
+		fmt.Printf("从服务端:%s,收到:%s\r\n", addr.String(), string(buf[:n]))
 	}
 }
