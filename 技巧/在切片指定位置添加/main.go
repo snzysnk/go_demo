829410f2b99b7eb791fd94810dd92e5a8a6c7c88
谢瑞祥 <1046421940@qq.com>
2024-05-16 16:17:41 +0800

    feat:增加在切片的指定位置增加元素

---
+++ 技巧/在切片指定位置添加/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+var (
+	example = []string{
+		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+	}
+	insertPosition = 4
+	insertValue    = "x"
+)
+
+func First() {
+	result := make([]string, 0)
+	result = append(result, example[:insertPosition]...)
+	result = append(result, insertValue)
+	result = append(result, example[insertPosition:]...)
+	fmt.Println(result)
+}
+
+func Next() {
+	example = append(example, "")
+
+	//底层array a b c d e  可见部分 f g h i j k l m  ""
+	// a b c d e e f g h i j k l m
+	copy(example[insertPosition+1:], example[insertPosition:])
+	example[insertPosition] = insertValue
+	fmt.Println(example)
+}
+
+func main() {
+	First() //[a b c d x e f g h i j k l m]
+	Next()  //[a b c d x e f g h i j k l m]
+}
