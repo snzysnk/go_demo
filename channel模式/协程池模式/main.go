8cc6516d90c710189e1331488dcf6db21e9632ee
xieruixiang <1046421940@qq.com>
2024-07-22 22:19:51 +0800

    complete chaneel model

--- channel模式/协程池模式/main.go
+++ channel模式/协程池模式/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 //TODO 完成协程池
 
 type TaskHandler func(p interface{})
@@ -10,10 +16,62 @@ type Task struct {
 	Handler TaskHandler
 }
 
+type WorkerPoolAbility interface {
+	AddWorker()
+	Release()
+	AddTask(task Task)
+}
+
 // WorkerPool 协程池结构
 type WorkerPool struct {
-	//队列缓冲大小
-	s int
-	//task 队列
-	q chan Task
+	ch chan Task
+	wg sync.WaitGroup
+}
+
+func (w *WorkerPool) AddTask(task Task) {
+	w.ch <- task
+}
+
+func (w *WorkerPool) AddWorker() {
+	w.wg.Add(1)
+	go func() {
+		for t := range w.ch {
+			time.Sleep(3 * time.Second)
+			t.Handler(t.Params)
+		}
+		w.wg.Done()
+	}()
+}
+
+func (w *WorkerPool) Release() {
+	close(w.ch)
+	w.wg.Wait()
+}
+
+func main() {
+	pool := WorkerPool{
+		ch: make(chan Task, 100),
+		wg: sync.WaitGroup{},
+	}
+	pool.AddWorker()
+	pool.AddWorker()
+	pool.AddTask(Task{
+		Params: "Hello",
+		Handler: func(p interface{}) {
+			fmt.Println(p)
+		},
+	})
+	pool.AddTask(Task{
+		Params: "World!",
+		Handler: func(p interface{}) {
+			fmt.Println(p)
+		},
+	})
+	pool.AddTask(Task{
+		Params: "满了!!!",
+		Handler: func(p interface{}) {
+			fmt.Println(p)
+		},
+	})
+	pool.Release()
 }
--- channel模式/管道模式/main.go
+++ channel模式/管道模式/main.go
@@ -1,3 +1,48 @@
 package main
 
-//TODO 管道模式
+import (
+	"fmt"
+	"strconv"
+)
+
+func jobStart() <-chan string {
+	outCh := make(chan string)
+	go func() {
+		for i := 0; i < 10; i++ {
+			outCh <- "工序begin" + strconv.Itoa(i)
+		}
+		close(outCh)
+	}()
+	return outCh
+}
+
+func jobB(inCh <-chan string) <-chan string {
+	outCh := make(chan string)
+	go func() {
+		for v := range inCh {
+			outCh <- v + "-工序B"
+		}
+		close(outCh)
+	}()
+	return outCh
+}
+
+func jobC(inCh <-chan string) <-chan string {
+	outCh := make(chan string)
+	go func() {
+		for v := range inCh {
+			outCh <- v + "-工序C"
+		}
+		close(outCh)
+	}()
+	return outCh
+}
+
+func main() {
+	ch := jobStart()
+	ch = jobB(ch)
+	ch = jobC(ch)
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
